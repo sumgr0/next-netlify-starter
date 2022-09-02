b29ba537b14bd53213630bbbeae3db9e2dff89f2
sumgr0 <sumit.grover@gmail.com>
2022-09-02 12:45:28 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to SubTko by @sumgr0!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Visit Twitter: <code>https://twitter.com/sumgr0</code>
         </p>
       </main>
 
